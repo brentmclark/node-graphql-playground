075a059a64941f67e3031c8af65ce7e05a21c655
Brent M Clark <brentmclark@gmail.com>
2019-01-04 14:18:11 -0600

    add main program entry point per instructions from ApolloServer

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+const port = 4000;
+
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
