71b7d117b984035fdf11c0949836653746049d15
Brent M Clark <brentmclark@gmail.com>
2019-01-04 15:18:17 -0600

    fetch character data from star wars API and pass through gql schema

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 /**
- * Taken from https://github.com/apollographql/apollo-server
+ * Started from https://github.com/apollographql/apollo-server
  */
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+require('isomorphic-fetch')
+
+async function getCharacter (id) {
+    const response = await fetch(`https://swapi.co/api/people/${id}`)
+    character = await response.json()
+    return character
+}
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
+    character(id: Int!): Character
+  }
+
+  type Character {
+    name: String
+    height: Int
+    mass: Int
+    hair_color: String
+    skin_color: String,
+    eye_color: String,
+    birth_year: String,
+    gender: String,
+    homeworld: String,
+    films: [String]
+    species: [String]
+    vehicles: [String]
+    starships: [String]
+    created: String
+    edited: String
+    url: String
   }
 `;
 
@@ -15,6 +42,9 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    character(parent, args, context, info) {
+        return getCharacter(args.id)
+    }
   },
 };
 
